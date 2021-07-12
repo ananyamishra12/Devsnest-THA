9a5ef8cabe4c0472af1bb69f7966492c8a758752
ananyamishra12 <66728375+ananyamishra12@users.noreply.github.com>
2021-07-13 03:55:13 +0530

    complete

---
+++ Day-12/script.js
@@ -0,0 +1,26 @@
+let addToDoButton= document.getElementById("addToDo");
+let toDoContainer= document.getElementById("todoContainer");
+let inputfield= document.getElementById("inputField");
+let clear=document.getElementById("clearAll");
+
+addToDoButton.addEventListener('click',myFun);
+inputfield.addEventListener('keyup',myFun){
+    
+}
+function myFun(){
+    var paragraph= document.createElement('p');//a paragraph element will be added every time we click the + button
+    paragraph.innerText= inputfield.value;//in that paragraph entered value in the text field has to be inputted
+    //nodeValue gives the text inside a tag. Here whatever tasks you will input in text that will be returned to paragraph
+    //value is used to set or return the value of the attribute of a text field
+    paragraph.classList.add('paragraph-styling');
+    toDoContainer.appendChild(paragraph);//adding that value in paragraph element
+    inputfield.value=" ";
+    paragraph.addEventListener("click",function(){
+        paragraph.style.textDecoration="line-through";
+    })
+    clear.addEventListener("click",function(){
+        paragraph.remove();
+    })
+    
+}
+
