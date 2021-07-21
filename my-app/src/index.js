fd33f48e01ad247a31f95490cae5b75cfa60aa53
ananyamishra12 <66728375+ananyamishra12@users.noreply.github.com>
2021-07-22 01:56:41 +0530

    day 15

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// ReactDOM.render(
+//     <h1>Hello World!!!!</h1>, document.getElementById('root')
 
+// );
 
+function Print(){
+    return (
+        <div>
+            <h1>
+                 Hello world
+            </h1>
+            <h1>
+                !!!!
+            </h1>
+        </div>    
+    );
+}
 
-ReactDOM.render(
-    <h1>Hello World</h1>, document.getElementById('root')
-
-);
+ReactDOM.render(<Print></Print>, document.getElementById('root'));
+console.log("hello");
