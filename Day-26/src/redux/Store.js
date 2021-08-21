80eabba11b9d24b3a163664e5b398b5d587897d9
ananyamishra12 <66728375+ananyamishra12@users.noreply.github.com>
2021-08-21 17:53:49 +0530

    edit

--- Day-26/src/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import { userFormReducer } from "./userForm/reducer";
-
-//It takes reducer function as parameter
-const store = createStore(userFormReducer);
-
-export default store;
- 
--- Day-26/src/redux/userForm/userFormActionType.js
+++
@@ -1,3 +0,0 @@
-//type
-export const updateFirstName = "updateFirstName";
-export const updateLastName = "updateLastName";
